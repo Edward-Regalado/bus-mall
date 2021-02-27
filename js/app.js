4ae1f1633cc2508196dd6c1a47540af07405a8d5
Edward Regalado <tregalado30@gmail.com>
2021-02-26 22:55:02 -0800

    added local storage

--- js/app.js
+++ js/app.js
@@ -99,6 +99,11 @@ function handleClick(event) {
   if (totalClicks === clicksAllowed) {
     myContainer.removeEventListener('click', handleClick);
     renderChart();
+<<<<<<< HEAD
+=======
+    // let stringifiedProducts = JSON.stringify(allProducts);
+    // localStorage.setItem('products', stringifiedProducts);
+>>>>>>> 56d2cab (fixing error)
   }
 }
 
